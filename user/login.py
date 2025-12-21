2788609c39b7a294a7c178be1964639c53961569
mohammad-mohammadi-cs-iust <m.mohammadi.cs@gmail.com>
2025-12-21 18:39:29 +0330

    updating login.py and new functions to the code

--- user/login.py
+++ user/login.py
@@ -1,47 +1,61 @@
-#import bcrypt library to use it for encoding user password.
 import bcrypt
-# import re library to check the regular expression(regex) of an email.
-import re
-
-#import json
 import json
-
-#import os
 import os
-
-#import uuid
-import uuid
 
 BASE_DIR = os.path.dirname(__file__)
 file_path = os.path.join(BASE_DIR, "users.json")
 
 
+def load_users():
+    try:
+        with open(file_path, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
+
+
+def find_username():
+    users = load_users()
+
+    while True:
+        input_username = input("\n-Please type the username: ").strip()
+
+        if not input_username:
+            print("Invalid username. Try again.")
+            continue
+
+        for user in users:
+            if user["username"] == input_username:
+                return input_username
+
+        print("Username not found. Please sign up first.")
+
+
+def check_password(username):
+    users = load_users()
+
+    while True:
+        input_password = input("\n-Please type the password: ").strip()
+        input_password_bytes = input_password.encode("utf-8")
+
+        for user in users:
+            if user["username"] == username:
+                stored_hash = user["password"].encode("utf-8")
+
+                if bcrypt.checkpw(input_password_bytes, stored_hash):
+                    return True
+                else:
+                    print("Incorrect password. Try again.")
+
+
 def login():
-    
     if not os.path.exists(file_path):
         print("No users registered.")
-        return None
+        return
 
-    username = input("Username: ").strip()
-    password = input("Password: ").strip().encode("utf-8")
+    username = find_username()
+    if check_password(username):
+        print(f"User '{username}' with logged in successfully!")
 
-    with open(file_path, "r", encoding="utf-8") as f:
-        try:
-            users = json.load(f)
-        except json.JSONDecodeError:
-            print("User database error.")
-            return None
 
-    for user in users:
-        if user["username"] == username:
-            stored_password = user["password"].encode("utf-8")
-
-            if bcrypt.checkpw(password, stored_password):
-                print("Login successful.")
-                return user
-            else:
-                print("Incorrect password.")
-                return None
-
-    print("Username not found.")
-    return None
+login()
