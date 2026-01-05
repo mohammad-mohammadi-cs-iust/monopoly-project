71f8a51dcf19ad29baf6ef771e560c3d9d5606f7
Mani <meighani1386@gmail.com>
2026-01-05 21:49:17 +0330

    add go and run_menu function

--- functions/main.py
+++ functions/main.py
@@ -4,71 +4,93 @@ from functions.transactions import *
 from functions.special import *
 from functions.graphicalmap import players_board
 from functions.jail import *
+from functions.game_end import *
+from functions.menu import *
 def dice():
     dice_1 = random.randint(1, 6)
     dice_2 = random.randint(1, 6)
     return dice_1 + dice_2, dice_1, dice_2
 repeat_dice = 0
 while True:
+    go()
+    run_menu()
     _, _, user = get_current_player()
-    if user['prison'] == 0:
-        total_dice, dice_1, dice_2 = dice()
-        if dice_1 == dice_2:
-            repeat_dice += 1
-            if repeat_dice == 3:
-                user['prison'] += 1
-                print('you are going to jail because you had 3 double')
-                user['position'] = 11
-                next_turn()
+    if user['status'] == 'In Progress':
+        if user['prison'] == 0:
+            total_dice, dice_1, dice_2 = dice()
+            if dice_1 == dice_2:
+                repeat_dice += 1
+                if repeat_dice == 3:
+                    user['prison'] += 1
+                    print('you are going to jail because You rolled the same dice three times in a row.')
+                    user['position'] = 11
+                    next_turn()
+                    repeat_dice = 0
 
-                repeat_dice = 0
-                continue
+                else:
+                    user['position'] += total_dice
+                    block = get_current_block()
+                    if 'owner' in block:
+                        if block['owner'] != '':
+                            if block['owner'] == user['username']:
+                                build_houses_and_hotels()
+                                sell_properties()
+
+                            else:
+                                resolve_bankrupt(user['position'], total_dice)
+                                sell_properties()
+
+                        else:
+                            ownership()
+                            sell_properties()
+
+                    elif block['name'] == 'Chance':
+                        chance_card()
+
+                    elif block['name'] == "Community Chest":
+                        community_chest_card()
+
+                    elif block['name'] == 'Income Tax' or block['name'] == 'Luxury Tax':
+                        if user['money'] >= block['amount']:
+                            user['money'] -= block['amount']
+                        else:
+                            resolve_bankrupt()
+
+                    elif block['name'] == 'Go To Jail':
+                        user['prison'] += 1
+                        user['position'] = 11
+                        print('you are going to jail because you are on go to jail block')
+                    
+
             else:
                 user['position'] += total_dice
-                block = get_current_block()
-                if 'owner' in block:
-                    if block['owner'] != '':
-                        if block['owner'] == user['username']:
-                            build_houses_and_hotels()
-                            sell_properties()
-                        else:
-                            resolve_bankrupt(user['position'], total_dice)
-                            sell_properties()
-                    else:
-                        ownership()
+                repeat_dice = 0
+                next_turn()
+                if block['owner'] != '':
+                    if block['owner'] == user['username']:
+                        build_houses_and_hotels()
                         sell_properties()
-                elif block['name'] == 'Chance':
-                    chance_card()
-                elif block['name'] == "Community Chest":
-                    community_chest_card()
-                elif block['name'] == 'Income Tax' or block['name'] == 'Luxury Tax':
-                    if user['money'] >= block['amount']:
-                        user['money'] -= block['amount']
+
                     else:
-                        resolve_bankrupt()
-                elif block['name'] == 'Go To Jail':
-                    user['prison'] += 1
-                    user['position'] = 11
-                    print('you are going to jail because you are on go to jail block')
-                
+                        sell_properties()
 
-                
-        else:
-            user['position'] += total_dice
-            repeat_dice = 0
-            next_turn()
-            if block['owner'] != '':
-                if block['owner'] == user['username']:
-                    build_houses_and_hotels()
-                    sell_properties()
                 else:
+                    ownership()
                     sell_properties()
-            else:
-                ownership()
-                sell_properties()
-    else:
-        manage_prison()
-    
 
+        else:
+            manage_prison()
+
+    else:
+        next_turn()
+    
+    save_players()
+    if end_game_if_finished() == "Game ended successfully. Returning to the menu....":
+        print("Game ended successfully. Returning to the menu....")
+        break
+    if input('do you want to continue?:').lower() == 'yes':
+        continue
+    else:
+        break
 
 
